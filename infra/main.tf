e5c05c418f05fc94042362e3a6a7338550b19bc8
jovandrobnjak <jovan.drobnjak@vegait.rs>
2024-05-08 14:20:26 +0200

    Remove unecessary definitions

--- infra/main.tf
+++ infra/main.tf
@@ -90,18 +90,3 @@ module "vpc" {
   create_igw             = true
 
 }
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "jd-terraform-state"
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "jd-terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
