7e97f03d76fcf8ad823c17a0256cddb2bfae2828
jovandrobnjak <jovan.drobnjak@vegait.rs>
2024-05-09 16:20:07 +0200

    Replicate Igor's config and import it

--- infra/security.tf
+++ infra/security.tf
@@ -1,52 +1,63 @@
 module "iam_policy" {
-  source      = "terraform-aws-modules/iam/aws//modules/iam-policy"
-  name        = "github_action_policy"
-  description = "Policy for pushing images into ECR with github action"
+  source = "terraform-aws-modules/iam/aws//modules/iam-policy"
+
+  name        = "JovanDGithubActionPolicy"
+  description = "Policy for pushing images into ECR with GitHub Actions"
+  path        = "/"
 
   policy = <<EOF
-  {
-    "Version" : "2012-10-17",
-    "Statement" : [
+{
+  "Version": "2012-10-17",
+  "Statement": [
       {
-        "Effect" : "Allow",
-        "Action" : [
-          "ecr:CompleteLayerUpload",
-          "ecr:GetAuthorizationToken",
-          "ecr:UploadLayerPart",
-          "ecr:InitiateLayerUpload",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:PutImage"
-        ],
-        "Resource" : "*"
+          "Effect": "Allow",
+          "Action": [
+              "ecr:CompleteLayerUpload",
+              "ecr:GetAuthorizationToken",
+              "ecr:UploadLayerPart",
+              "ecr:InitiateLayerUpload",
+              "ecr:BatchCheckLayerAvailability",
+              "ecr:PutImage"
+          ],
+          "Resource": "*"
       }
-    ]
+  ]
+}
+EOF
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
+
+  tags = {
+    Owner = "Igor Kostin"
   }
-  EOF
 }
-module "iam_github_oidc_provider" {
-  count  = data.aws_iam_openid_connect_provider.oidc_provider.arn == null ? 1 : 0
-  source = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
-
+import {
+  id = "arn:aws:iam::162340708442:oidc-provider/token.actions.githubusercontent.com"
+  to = aws_iam_openid_connect_provider.github
 }
-
 module "iam_assumable_role_with_oidc" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  depends_on = [module.iam_policy]
+  source     = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
 
   oidc_subjects_with_wildcards = ["repo:jovandrobnjak/devops-training-docker-nodejs-sample:*"]
-  create_role                  = true
 
-  role_name = "jovand-github-action"
+  create_role = true
+
+  role_name = "JovanDGithubActionRole"
 
   tags = {
     Role = "role-with-oidc"
   }
 
-  provider_url = data.aws_iam_openid_connect_provider.oidc_provider.url == null ? module.iam_github_oidc_provider.url : data.aws_iam_openid_connect_provider.oidc_provider.url
+  provider_url = aws_iam_openid_connect_provider.github.url
 
-  role_policy_arns = [
-    module.iam_policy.arn
-  ]
+  number_of_role_policy_arns = 1
+
+  role_policy_arns = [module.iam_policy.arn]
 
   oidc_fully_qualified_audiences = ["sts.amazonaws.com"]
-  number_of_role_policy_arns     = 1
 }
--- infra/variable.tf
+++ infra/variable.tf
@@ -1,7 +1,3 @@
 data "aws_availability_zones" "available_zones" {
   state = "available"
 }
-
-data "aws_iam_openid_connect_provider" "oidc_provider" {
-  url = "https://token.actions.githubusercontent.com"
-}
