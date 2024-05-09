6067b9aaf66b1390217277f8caa8915742879840
jovandrobnjak <jovan.drobnjak@vegait.rs>
2024-05-09 14:52:12 +0200

    Implement with already existing provider

--- infra/security.tf
+++ infra/security.tf
@@ -23,10 +23,6 @@ module "iam_policy" {
   }
   EOF
 }
-module "iam_github_oidc_provider" {
-  source = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
-
-}
 
 module "iam_assumable_role_with_oidc" {
   source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
@@ -40,7 +36,7 @@ module "iam_assumable_role_with_oidc" {
     Role = "role-with-oidc"
   }
 
-  provider_url = module.iam_github_oidc_provider.url
+  provider_url = data.aws_iam_openid_connect_provider.oidc_provider.url
 
   role_policy_arns = [
     module.iam_policy.arn
