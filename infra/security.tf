68d747d6f3e4ebf1dacd79fe7261004f3b0b849b
jovandrobnjak <jovan.drobnjak@vegait.rs>
2024-05-13 12:58:16 +0200

    Add service account name

--- infra/security.tf
+++ infra/security.tf
@@ -65,7 +65,7 @@ module "iam_eks_role" {
   oidc_providers = {
     main = {
       provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["default"]
+      namespace_service_accounts = ["default:devops-training-docker-nodejs-sample"]
     }
   }
 }
