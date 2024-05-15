4c3dedcc1a06a42e3c7739a5c6e3e661caf958df
jovandrobnjak <jovan.drobnjak@vegait.rs>
2024-05-15 10:08:58 +0200

    Add helm provider

--- infra/main.tf
+++ infra/main.tf
@@ -12,6 +12,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.48.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.13.2"
+    }
   }
 }
 
@@ -25,3 +29,15 @@ provider "aws" {
   }
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
+  }
+}
