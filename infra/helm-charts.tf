02fcd3e7081da66a4dec2943258c33e6c0477995
jovandrobnjak <jovan.drobnjak@vegait.rs>
2024-05-15 10:47:11 +0200

    Add load balancer controller helm chart

---
+++ infra/helm-charts.tf
@@ -0,0 +1,27 @@
+# helm install aws-load-balancer-controller eks/aws-load-balancer-controller --set clusterName=jovand-cluster -n kube-system --set serviceAccount.create=false --set serviceAccount.name=devops-training-docker-nodejs-sample
+
+resource "helm_release" "load_balancer_controller" {
+  name       = "jovand-loadbalancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "default"
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "devops-training-docker-nodejs-sample"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.iam_eks_role.iam_role_arn
+  }
+}
