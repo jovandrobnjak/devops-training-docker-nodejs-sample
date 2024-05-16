58ca048c9c0b589e98389fbb12e3d0ee427eb201
jovandrobnjak <jovan.drobnjak@vegait.rs>
2024-05-16 11:13:18 +0200

    Remove value of secret version id

--- infra/secrets.tf
+++ infra/secrets.tf
@@ -14,6 +14,5 @@ module "secrets_manager" {
 
 
 data "aws_secretsmanager_secret_version" "current" {
-  secret_id  = module.secrets_manager.secret_id
-  version_id = module.secrets_manager.secret_version_id
+  secret_id = module.secrets_manager.secret_id
 }
