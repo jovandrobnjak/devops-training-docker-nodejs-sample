6129c3f0813e80359db296b7e2f9206fe151d33c
jovandrobnjak <jovan.drobnjak@vegait.rs>
2024-05-23 14:25:05 +0200

    Try different acction

--- infra/eks.tf
+++ infra/eks.tf
@@ -19,6 +19,11 @@ module "eks" {
       resolve_conflicts        = "PRESERVE"
     }
   }
+  access_entries = {
+    github = {
+      principal_arn = module.iam_assumable_role_with_oidc.iam_role_arn
+    }
+  }
 
   eks_managed_node_groups = {
     jovand-node-group = {
