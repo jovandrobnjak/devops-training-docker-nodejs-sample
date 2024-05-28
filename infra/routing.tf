660e9aa2a7f984d1180f8295aa2b32901fcf53a0
jovandrobnjak <jovan.drobnjak@vegait.rs>
2024-05-28 11:17:34 +0200

    Add depends_on

--- infra/routing.tf
+++ infra/routing.tf
@@ -35,5 +35,6 @@ data "kubernetes_service" "data_load_balancer" {
 }
 
 data "aws_lb" "load_balancer_source" {
-  name = "k8s-vegaittr-jovandto-95ab8df602"
+  depends_on = [helm_release.load_balancer_controller]
+  name       = "k8s-vegaittr-jovandto-95ab8df602"
 }
